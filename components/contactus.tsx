05de629eba8ac58f2f588f3236610f0f53f5817b
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-10 22:12:56 -0700

    Modify id type

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -16,7 +16,7 @@ const ContactUs: React.FC = () => {
 	});
 
 	const handleOnChange = useCallback(
-		(e: { persist: () => void; target: { id: any; value: any } }) => {
+		(e: { persist: () => void; target: { id: number; value: any } }) => {
 			e.persist();
 			setInputs((prev) => ({
 				...prev,
