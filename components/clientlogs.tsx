69f3f68b81441dc42d98ace8ce8054fd78209efb
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-11 10:57:13 -0700

    Update logos

--- components/clientlogs.tsx
+++ components/clientlogs.tsx
@@ -5,51 +5,67 @@ import Image from 'next/image';
 const ClientLogs: React.FC = () => (
 	<>
 		<SliderContainer className='' contentWidth={1290} initialOffSet={0}>
-			<SliderItem width={150}>
-				<Image
-					src='/assets/customer/safeway.ca.png'
-					width={150}
-					height={50}
-					alt='safeway'
-					objectFit='contain'
-				/>
-			</SliderItem>
-			<SliderItem width={200}>
-				<Image
-					src='/assets/customer/safeway.ca.png'
-					width={200}
-					height={50}
-					alt='safeway'
-					objectFit='contain'
-				/>
-			</SliderItem>
-			<SliderItem width={150}>
-				<Image
-					src='/assets/customer/safeway.ca.png'
-					width={150}
-					height={50}
-					alt='safeway'
-					objectFit='contain'
-				/>
-			</SliderItem>
-			<SliderItem width={150}>
-				<Image
-					src='/assets/customer/safeway.ca.png'
-					width={150}
-					height={50}
-					alt='safeway'
-					objectFit='contain'
-				/>
-			</SliderItem>
-			<SliderItem width={150}>
-				<Image
-					src='/assets/customer/safeway.ca.png'
-					width={150}
-					height={50}
-					alt='safeway'
-					objectFit='contain'
-				/>
-			</SliderItem>
+			<Image
+				src='/assets/customer/const.png'
+				width={150}
+				height={50}
+				alt='const'
+				objectFit='contain'
+			/>
+
+			<Image
+				src='/assets/customer/sun.png'
+				width={150}
+				height={50}
+				alt='sun'
+				objectFit='contain'
+			/>
+			<Image
+				src='/assets/customer/point.png'
+				width={150}
+				height={50}
+				alt='point'
+				objectFit='contain'
+			/>
+			<Image
+				src='/assets/customer/surge.png'
+				width={150}
+				height={50}
+				alt='surge'
+				objectFit='contain'
+			/>
+
+			<Image
+				src='/assets/customer/eo.png'
+				width={150}
+				height={50}
+				alt='eo'
+				objectFit='contain'
+			/>
+
+			<Image
+				src='/assets/customer/hono.png'
+				width={150}
+				height={50}
+				alt='hono'
+				objectFit='contain'
+			/>
+
+			<Image
+				src='/assets/customer/acidus.png'
+				width={150}
+				height={50}
+				alt='acidus'
+				objectFit='contain'
+			/>
+
+			<Image
+				src='/assets/customer/rab.png'
+				width={150}
+				height={50}
+				alt='rab'
+				objectFit='contain'
+			/>
 		</SliderContainer>
 	</>
 );
