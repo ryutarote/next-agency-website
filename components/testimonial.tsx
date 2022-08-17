0d759523b810097f2dc6921a0a6dc484075a318c
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-17 11:39:21 -0700

    Fix quote issue

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -7,32 +7,32 @@ const Testimonial: React.FC = () => (
 	<Carousel className='bg-black text-white py-10 lg:py-20'>
 		<CarouselItem index={0}>
 			<Review by='Alex (folks)'>
-				"Burnavy were aware of our potential problems and solved our burning
-				issue."
+				&ldquo;Burnavy were aware of our potential problems and solved our
+				burning issue.&rdquo;
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={1}>
 			<Review by='Ruke (studyio)'>
-				“Our company is no ordinary education services. In one summer, we taught
-				our customer to grow and got jobs.”
+				&ldquo;Our company is no ordinary education services. In one summer, we
+				taught our customer to grow and got jobs.&rdquo;
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={2}>
 			<Review by='Jacob (Kingstone)'>
-				"Burnavy got rid of our pain and conceren. We are no longer fail to
-				proceed to our business"
+				&ldquo;Burnavy got rid of our pain and conceren. We are no longer fail
+				to proceed to our business.&rdquo;
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={3}>
 			<Review by='Slave (Shinees)'>
-				“How to solve our issue with growing on the market was explained
-				promptly and politely. Excellent service.”
+				&ldquo;How to solve our issue with growing on the market was explained
+				promptly and politely. Excellent service.&rdquo;
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={4}>
 			<Review by='Folister (Quest animals)'>
-				“Burnavy's relentless pursuit of knowledge and touring in the busines
-				has led to countless journey all across the globe.”
+				&ldquo;“Burnavy's relentless pursuit of knowledge and touring in the
+				busines has led to countless journey all across the globe.”&rdquo;
 			</Review>
 		</CarouselItem>
 	</Carousel>
