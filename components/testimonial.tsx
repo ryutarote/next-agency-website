5a515090a058de4046ab0467df01e832a4619557
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-17 12:08:22 -0700

    Fix apostrophy

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -7,26 +7,26 @@ const Testimonial: React.FC = () => (
 	<Carousel className='bg-black text-white py-10 lg:py-20'>
 		<CarouselItem index={0}>
 			<Review by='Alex (folks)'>
-				&ldquo;Burnavy were aware of our potential problems and solved our
-				burning issue.&rdquo;
+				Burnavy were aware of our potential problems and solved our burning
+				issue.
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={1}>
 			<Review by='Ruke (studyio)'>
-				&ldquo;Our company is no ordinary education services. In one summer, we
-				taught our customer to grow and got jobs.&rdquo;
+				Our company is no ordinary education services. In one summer, we taught
+				our customer to grow and got jobs.
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={2}>
 			<Review by='Jacob (Kingstone)'>
-				&ldquo;Burnavy got rid of our pain and conceren. We are no longer fail
-				to proceed to our business.&rdquo;
+				Burnavy got rid of our pain and conceren. We are no longer fail to
+				proceed to our business.
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={3}>
 			<Review by='Slave (Shinees)'>
-				&ldquo;How to solve our issue with growing on the market was explained
-				promptly and politely. Excellent service.&rdquo;
+				How to solve our issue with growing on the market was explained promptly
+				and politely. Excellent service.
 			</Review>
 		</CarouselItem>
 		<CarouselItem index={4}>
