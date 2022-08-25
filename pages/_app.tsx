1c7b27ae3cd7b14ad0c0d3ecb4c6e3e254bfbb6f
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-25 12:28:07 -0700

    Add formspree packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import ScrollObserver from '../utils/scroll-observer';
 import SizeObeserver from '../utils/size-observer';
+import { FormspreeProvider } from '@formspree/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
 		<SizeObeserver>
 			<ScrollObserver>
-				<Component {...pageProps} />
+				<FormspreeProvider project='contactform'>
+					<Component {...pageProps} />
+				</FormspreeProvider>
 			</ScrollObserver>
 		</SizeObeserver>
 	);
