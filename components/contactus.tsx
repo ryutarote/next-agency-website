7a671e2f18929500533821240f144eee450b63c2
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-08-26 10:29:12 -0700

    Check if it works

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -58,7 +58,7 @@ const ContactUs: React.FC = () => {
 			setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
 			axios({
 				method: 'POST',
-				url: 'https://formspree.io/f/mjvzvnjk',
+				url: process.env.NEXT_PUBLIC_CONTACT_FORM_ENDPOINT_URL,
 				data: inputs,
 			}).then((_response) => {
 				handleServerResponse(
