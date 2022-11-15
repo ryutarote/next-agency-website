7e444fb226ffa1a24f91acc22590df0575d34583
ryutarote <75284855+ryutarote@users.noreply.github.com>
2022-11-15 08:08:16 -0800

    Refactor

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Footer: React.FC = () => (
-	<footer className='flex gap-8 items-center justify-center bg-black text-white p-20'>
+	<footer className='min-h-full flex flex-wrap gap-8 items-center justify-center bg-black text-white px-4 py-20'>
 		<Image src='/assets/logo.png' width={30} height={30} alt='logo'></Image>
 		<Link href='/terms'>Terms</Link>
 		<Link href='/privacy'>Privacy policy</Link>
